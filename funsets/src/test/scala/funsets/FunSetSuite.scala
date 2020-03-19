0b7246cbd767aacada6a69733f378b42ab4a4ef0
Jakub Brzegowski <symfiz@gmail.com>
2020-03-19 23:03:27 +0100

    Add solution to funsets

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -47,7 +47,7 @@ class FunSetSuite {
    * Once you finish your implementation of "singletonSet", remvoe the
    * @Ignore annotation.
    */
-  @Ignore("not ready yet") @Test def `singleton set one contains one`: Unit = {
+  @Test def `singleton set one contains one`: Unit = {
 
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -58,7 +58,9 @@ class FunSetSuite {
        * The string argument of "assert" is a message that is printed in case
        * the test fails. This helps identifying which assertion failed.
        */
-      assert(contains(s1, 1), "Singleton")
+      assert(contains(s1, 1), "Singleton 1")
+      assert(contains(s2, 2), "Singleton 2")
+      assert(contains(s3, 3), "Singleton 3")
     }
   }
 
