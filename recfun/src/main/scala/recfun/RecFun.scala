7ca9ebb5535ef39eaca154c254c7be3a3476199c
Jakub Brzegowski <symfiz@gmail.com>
2020-03-19 23:07:00 +0100

    Add solution to recfun

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -14,15 +14,37 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = (c, r) match {
+    case _ if (c == 0 || r == c) => 1
+    case _ => pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceRec(chs: List[Char], lCount: Int, rCount: Int): Boolean = chs match {
+      case Nil => lCount == rCount
+      case ch::tail =>
+        val lIncrease = if (ch == '(') 1 else 0
+        val rIncrease = if (ch == ')') 1 else 0
+
+        if (lCount < rCount) false
+        else balanceRec(tail, lCount + lIncrease, rCount + rIncrease)
+    }
+
+    balanceRec(chars, 0, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    // Can be way simpler -> use if else
+    coins.fold(0)((acc, coin) => acc + (money - coin match {
+      case _ if (money - coin < 0) => 0
+      case 0 => 1
+      case _ => countChange(money - coin, coins.filter(n => n >= coin))
+    }))
+  }
 }
